33eb799d4e3aaabdc0711600155750bd6653c443
FelixChen1 <felix.chen.zhuhai@softwre.dell.com>
2016-07-20 01:17:01 +0800

    Add few implements

---
+++ IDisposableSample/Unmanaged/DisposableObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Unmanaged
+{
+    class DisposableObject
+    {
+        private MemoryStream _stream;
+
+        public DisposableObject()
+        {
+            Console.WriteLine("DisposableObject: constructor, creating memory stream...");
+            _stream = new MemoryStream();
+        }
+
+        public void SayMorning()
+        {
+            Console.WriteLine("Good morning!");
+        }
+
+        #region IDisposable Support
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                Console.WriteLine("DisposableObject: object have been disposed.");
+                return;
+            }
+
+            if (disposing)
+            {
+                Console.WriteLine("DisposableObject: releasing unmanaged resource...");
+                _stream.Close();
+            }
+            Console.WriteLine("DisposableObject: object disposed.");
+            disposed = true;
+        }
+
+        ~DisposableObject()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
